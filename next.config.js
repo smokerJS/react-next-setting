6958a5b5d0442e4346edf5b4d0c9c19a9a7ba3ad
Jung, Jinsol <smokerjs.dev@gmail.com>
2019-10-14 16:03:27 +0900

    file loader 추가

--- next.config.js
+++ next.config.js
@@ -12,8 +12,21 @@ const NextAppConfig = {
             ...(config.resolve.alias || {}),
             '@styles': path.join(__dirname, 'assets/styles'),
             '@layout': path.join(__dirname, 'layout'),
+            '@images': path.join(__dirname, 'static/images'),
             '@': path.join(__dirname, 'components'),
         }
+        config.module.rules = [
+            ...config.module.rules,
+            ...[{
+                test: /\.(png|woff|woff2|eot|ttf|gif|jpg|ico|svg)$/,
+                loader: 'file-loader',
+                options: {
+                name: '[name]_[hash].[ext]',
+                publicPath: `/_next/static/files`,
+                outputPath: 'static/files'
+                }
+            }]
+        ]
         return config;
     },
 };
