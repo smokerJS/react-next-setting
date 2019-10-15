42a264aa457e0cd30ed85efcb5f36b0466126825
Jung, Jinsol <smokerjs.dev@gmail.com>
2019-10-16 00:33:24 +0900

    components fix

--- components/Hello.js
+++ components/Hello.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
 const Hello = () => (
-    <React.Fragment>
-        <div className="index-hello">Hello!</div>
-        <img src={require('@images/img-test.png')}/>
-    </React.Fragment>
-)
+  <>
+    <div className="index-hello">Hello!</div>
+    <img src={require('@images/img-test.png')} />
+  </>
+);
 
 export default Hello;
