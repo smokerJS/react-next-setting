aa841eaf8180d047a367ed6ea38dd6b3ccc0f1a9
Jung, jin sol <smokerjs.dev@gmail.com>
2021-01-08 15:48:20 +0900

    데코레이터 사용

--- stores/TestStore.ts
+++ stores/TestStore.ts
@@ -1,4 +1,4 @@
-import { flow, action, makeAutoObservable } from 'mobx';
+import { flow, action, observable } from 'mobx';
 import axios from 'axios';
 import BaseStore from './BaseStore';
 
@@ -11,20 +11,19 @@ export const initialState: InitialState = {
 };
 
 class TestStore extends BaseStore<InitialState> {
+  @observable
   test!: number;
 
   constructor(initialData: InitialState) {
     super();
     this.init(initialData);
-    makeAutoObservable(this, {
-      action: action.bound,
-    });
   }
 
   init = (initialData: InitialState): void => {
     this.test = initialData.test;
   };
 
+  @action.bound
   action = flow(function* generator(this: TestStore) {
     const data = yield axios.get(
       'https://api.ping2g.com/sp/showrooms?from=home&home_YN=true&_sort=id:desc&_limit=10&id_lte=92'
